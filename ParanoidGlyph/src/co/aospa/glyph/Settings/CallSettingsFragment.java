f023681adccfbe1b6b12a7783b75bb618591b51f
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-03-07 09:20:13 +0100

    ParanoidGlyph: Better handle cases where requested animation doesn't exist
     * also improve asset and resource handling ResourceUtils
    
    Change-Id: I7144b0b767567b7887d7321d97d94d7cc07c3e29

--- ParanoidGlyph/src/co/aospa/glyph/Settings/CallSettingsFragment.java
+++ ParanoidGlyph/src/co/aospa/glyph/Settings/CallSettingsFragment.java
@@ -27,6 +27,7 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceScreen;
 
+import com.android.internal.util.ArrayUtils;
 import com.android.settingslib.widget.MainSwitchPreference;
 
 import co.aospa.glyph.R;
@@ -64,6 +65,9 @@ public class CallSettingsFragment extends PreferenceFragment implements Preferen
         mListPreference.setOnPreferenceChangeListener(this);
         mListPreference.setEntries(ResourceUtils.getCallAnimations());
         mListPreference.setEntryValues(ResourceUtils.getCallAnimations());
+        if (!ArrayUtils.contains(ResourceUtils.getCallAnimations(), mListPreference.getValue())) {
+            mListPreference.setValue(ResourceUtils.getString("glyph_settings_call_animations_default"));
+        }
 
         mGlyphAnimationPreference = (GlyphAnimationPreference) findPreference(Constants.GLYPH_CALL_SUB_PREVIEW);
     }
