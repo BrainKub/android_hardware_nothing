52da868e948dfbaa84698203714a1ce120e8648a
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-03-07 12:10:37 +0100

    ParanoidGlyph: Hide and disable Charging meter if not supported
     * make sure ServiceUtils will never start ChargingService in that case
     * also hide category if Powershare is disabled as well
    
    Change-Id: Ie200bf47e4414c17e81cb88ad99961f833a3c9ba

--- ParanoidGlyph/src/co/aospa/glyph/Utils/ServiceUtils.java
+++ ParanoidGlyph/src/co/aospa/glyph/Utils/ServiceUtils.java
@@ -41,6 +41,9 @@ public final class ServiceUtils {
 
     private static Context context = Constants.CONTEXT;
 
+    private static final int CHARGING_LEVELS = ResourceUtils.getInteger("glyph_settings_battery_levels_num");
+    private static boolean glyphChargingMeterAvailable = CHARGING_LEVELS > 0;
+
     private static final String POWERSHARE_ACTIVE = ResourceUtils.getString("glyph_settings_paths_powershare_active_absolute");
     private static final String POWERSHARE_ENABLED = ResourceUtils.getString("glyph_settings_paths_powershare_enabled_absolute");
     private static boolean glyphPowershareAvailable = !POWERSHARE_ACTIVE.isBlank() && !POWERSHARE_ENABLED.isBlank();
@@ -59,6 +62,7 @@ public final class ServiceUtils {
 
     private static void startChargingService() {
         if (DEBUG) Log.d(TAG, "Starting Glyph charging service");
+        if (!glyphChargingMeterAvailable) return;
         context.startServiceAsUser(new Intent(context, ChargingService.class),
                 UserHandle.CURRENT);
     }
